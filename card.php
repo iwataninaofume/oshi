2eccfc96ed1f21ddfebeaefe22359e9320ae6008
FERNANDO SANSAO SIQUEIRA <fernando.0913@aluno.pr.senac.br>
2023-11-20 17:23:02 -0300

    att

--- card.php
+++ card.php
@@ -1,69 +1,68 @@
-<div class="row"> <!-- A class row deixa os elementos em linha -->
-    <div class="col-3">
-        <div class="card" style="width: 18rem;">
-            <img src="" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Tokyo manji gangue</h5>
-                <p class="card-text">Essa e a gangue mangi de tokyo</p>
-                <a href="tokyo.html" class="btn btn-primary">vá conhece-los mais</a>
+<div class="container">
+    <div class="row"> <!-- inicio do row 1 -->
+        <div class="col-4">
+            <div class="card" style="width: 18rem;">
+                <img src="img/kanagawa.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Kanagawa</h5>
+                    <p class="card-text"></p>
+                    <a href="tokyo.html" class="btn btn-primary">Veja mais</a>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="col-3">
-        <div class="card" style="width: 18rem;">
-            <img src="../pastacard.html/img/rez.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">RE:Zero</h5>
-                <p class="card-text">seríe de anime e manga,RE:zero</p>
-                <a href="#" class="btn btn-primary">vá conhece-los mais</a>
+        <div class="col-4">
+            <div class="card" style="width: 18rem;">
+                <img src="img/tokyo.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Tokyo</h5>
+                    <p class="card-text"></p>
+                    <a href="#" class="btn btn-primary">Veja mais</a>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="col-3">
-        <div class="card" style="width: 18rem;">
-            <img src="../pastacard.html/img/naofume.png" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Top 2</h5>
-                <p class="card-text"></p>
-                <a href="#" class="btn btn-primary">Vá conhece-los mais</a>
+        <div class="col-4">
+            <div class="card" style="width: 18rem;">
+                <img src="img/tokyo2.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Tokyo</h5>
+                    <p class="card-text"></p>
+                    <a href="#" class="btn btn-primary">Veja mais</a>
+                </div>
             </div>
         </div>
-    </div>
-</div> <!-- FIM do row 1-->
+    </div><!-- FIM do row 1-->
 
-<div class="row"><!--inicio do row 1-->
-    <div class="col-3">
-        <div class="card" style="width: 18rem;">
-            <img src="../pastacard.html/img/luffy.jpe" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Top 4</h5>
-                <p class="card-text">Seríe de anime e manga One Piece</p>
-                <a href="#" class="btn btn-primary">Vá conhece-los mais</a>
+    <div class="row"><!-- inicio do row 2 -->
+        <div class="col-4">
+            <div class="card" style="width: 18rem;">
+                <img src="img/pico.avif" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Tokyo</h5>
+                    <p class="card-text"></p>
+                    <a href="#" class="btn btn-primary">Veja mais</a>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="col-3">
-        <div class="card" style="width: 18rem;">
-            <img src="../pastacard.html/img/kazuma.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Konosuba</h5>
-                <p class="card-text">Seríe de anime e manga Koonosuba</p>
-                <a href="#" class="btn btn-primary">Vá conhece-los mais</a>
+        <div class="col-4">
+            <div class="card" style="width: 18rem;">
+                <img src="img/hotel.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Tokyo</h5>
+                    <p class="card-text"></p>
+                    <a href="#" class="btn btn-primary">Veja mais</a>
+                </div>
             </div>
         </div>
-    </div>
-</div>
-</div>
-<div class="col-3">
-    <div class="card" style="width: 18rem;">
-        <img src="../pastacard.html/img/kazuma.jpg" class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">Konosuba</h5>
-            <p class="card-text">Seríe de anime e manga Koonosuba</p>
-            <a href="#" class="btn btn-primary">Vá conhece-los mais</a>
+        <div class="col-4">
+            <div class="card" style="width: 18rem;">
+                <img src="img/chinatown.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Chinatow</h5>
+                    <p class="card-text"></p>
+                    <a href="#" class="btn btn-primary">Veja mais</a>
+                </div>
+            </div>
         </div>
-    </div>
+    </div><!-- fim do row 2 -->
 </div>
-</div>
-</div><!--fim do row 2-->
 </body>
--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="estilo.css">
+    <link rel="stylesheet" href="panama.css">
 </head>
 <body>
     <h1>japão no outono</h1>
@@ -13,9 +13,10 @@
     include("menu.php");
     ?>
     <img src="img/japao.jpg" alt="">
-    
+    <div class="espaco"></div>
     <?php
-    include("card.php"); ?>
+    include("card.php"); 
+    ?>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </html>
