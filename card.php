11fc89454f9dd9a2fdb3c0f5a9aa1aa28e3560cd
FERNANDO SANSAO SIQUEIRA <fernando.0913@aluno.pr.senac.br>
2023-11-20 16:12:04 -0300

    jksdfkjdjfk

---
+++ card.php
@@ -0,0 +1,69 @@
+<div class="row"> <!-- A class row deixa os elementos em linha -->
+    <div class="col-3">
+        <div class="card" style="width: 18rem;">
+            <img src="" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Tokyo manji gangue</h5>
+                <p class="card-text">Essa e a gangue mangi de tokyo</p>
+                <a href="tokyo.html" class="btn btn-primary">vá conhece-los mais</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-3">
+        <div class="card" style="width: 18rem;">
+            <img src="../pastacard.html/img/rez.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">RE:Zero</h5>
+                <p class="card-text">seríe de anime e manga,RE:zero</p>
+                <a href="#" class="btn btn-primary">vá conhece-los mais</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-3">
+        <div class="card" style="width: 18rem;">
+            <img src="../pastacard.html/img/naofume.png" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Top 2</h5>
+                <p class="card-text"></p>
+                <a href="#" class="btn btn-primary">Vá conhece-los mais</a>
+            </div>
+        </div>
+    </div>
+</div> <!-- FIM do row 1-->
+
+<div class="row"><!--inicio do row 1-->
+    <div class="col-3">
+        <div class="card" style="width: 18rem;">
+            <img src="../pastacard.html/img/luffy.jpe" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Top 4</h5>
+                <p class="card-text">Seríe de anime e manga One Piece</p>
+                <a href="#" class="btn btn-primary">Vá conhece-los mais</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-3">
+        <div class="card" style="width: 18rem;">
+            <img src="../pastacard.html/img/kazuma.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Konosuba</h5>
+                <p class="card-text">Seríe de anime e manga Koonosuba</p>
+                <a href="#" class="btn btn-primary">Vá conhece-los mais</a>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+<div class="col-3">
+    <div class="card" style="width: 18rem;">
+        <img src="../pastacard.html/img/kazuma.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Konosuba</h5>
+            <p class="card-text">Seríe de anime e manga Koonosuba</p>
+            <a href="#" class="btn btn-primary">Vá conhece-los mais</a>
+        </div>
+    </div>
+</div>
+</div>
+</div><!--fim do row 2-->
+</body>
--- menu.php
+++ menu.php
@@ -1,10 +1,40 @@
-<div class="dropdown">
-  <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-    Dropdown
-  </button>
-  <ul class="dropdown-menu">
-    <li><button class="dropdown-item" type="button">Action</button></li>
-    <li><button class="dropdown-item" type="button">Another action</button></li>
-    <li><button class="dropdown-item" type="button">Something else here</button></li>
-  </ul>
-</div>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <img src="img/png.jpg"width="40" alt="">
+      <a class="navbar-brand" href="#">Top 5 - animes by Fernando</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Início</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contato</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              animes
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Passagens</a></li>
+              <li><a class="dropdown-item" href="#">Preços</a></li>
+              <li><a class="dropdown-item" href="#">Hoteis</a></li>
+              <li><a class="dropdown-item" href="#">One Piece</a></li>
+              <li><a class="dropdown-item" href="#"></a></li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
+              <li><a class="dropdown-item" href="#">Teste</a></li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+          </li>
+        </ul>
+
+      </div>
+    </div>
+  </nav>
